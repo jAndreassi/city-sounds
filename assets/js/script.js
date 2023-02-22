3e765ca0fc7f4b64534ccd774fe25e7206bbcfab
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-02-22 10:32:02 -0500

    pulls data from Deezer API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,17 @@
 // FUNCTIONS
 // USER INTERACTIONS
 // INITIALIZATION
+
+
+
+// Testing Deezer API
+
+var deezerPlaylistURL = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/user/637006841/playlists";
+
+fetch(deezerPlaylistURL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
